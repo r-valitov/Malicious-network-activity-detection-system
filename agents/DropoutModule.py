6f5e6e4700576f15ab3c5337a4b40ec594146cba
Rishat Valitov <valitov.rishat@gmail.com>
2020-05-24 00:26:31 +0300

    [WIP] Dropout module fix

--- agents/DropoutModule.py
+++ agents/DropoutModule.py
@@ -12,6 +12,7 @@ class DropoutModule(nn.Module):
         self.type = env_type
 
     def forward(self, state):
+        state = state.cuda()
         if self.type == -1:
             return state
         tenzor_type = state.dtype
@@ -25,34 +26,13 @@ class DropoutModule(nn.Module):
             else:
                 filler = 1
             mask[i] = (i + filler) % 2
+        mask = mask.cuda()
         return state * mask
-
-    # def forward(self, state):
-    #     if self.type == -1:
-    #         return state
-    #     filler = 0
-    #     tenzor_type = state.dtype
-    #     mask = torch.zeros(self.hidden_size, dtype=tenzor_type)
-    #     common = int(self.hidden_size/2)
-    #     for i in range(0, common):
-    #         mask[i] = 1
-    #     differ = common + int(self.hidden_size/4)
-    #     if self.type == 0:
-    #         filler = 1
-    #     for i in range(common, differ):
-    #         mask[i] = filler
-    #     if self.type == 0:
-    #         filler = 0
-    #     else:
-    #         filler = 1
-    #     for i in range(differ, self.hidden_size):
-    #         mask[i] = filler
-    #     return state * mask
 
 
 def test_dropout():
     hidden_size = 128
-    a = DropoutModule(env_type=1, hidden_size=hidden_size)
+    a = DropoutModule(env_type=0, hidden_size=hidden_size)
     random_tensor_one_ex = (torch.rand(hidden_size) * 10).int()
     answer = a.forward(random_tensor_one_ex)
     print(answer)
